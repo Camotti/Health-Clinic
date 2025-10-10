f0b34a82fd828fea546b853e81a018294bcd878d
Camotti <camiropi97@gmail.com>
2025-10-09 19:29:45 -0500

    console ui

--- utils/consoleUI.cs
+++ utils/consoleUI.cs
@@ -2,10 +2,27 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using healthclinic.Interfaces;
+using healthclinic.Services;
 
 public class ConsoleUI
 {
     private readonly IPatientService _patientService;
     private readonly IveterinarianRepository _veterinarianService;
     private readonly Validator _validator; 
+
+
+
+
+    // constructor 
+
+    public ConsoleUI(
+        IPatientService patientService,
+        IveterinarianRepository iveterinarianRepository,
+        _validator validator )
+
+    {
+        _patientService = patientService;
+        _veterinarianService = veterinarianService,
+        _validator = validator;
+    }
 }
